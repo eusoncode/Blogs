3a70955ca16f7308ce059e85f0f802b73cfbb45c
eusoncode <eusoncode@gmail.com>
2025-02-23 00:57:02 -0700

    Added Comment models

--- blogs/models.py
+++ blogs/models.py
@@ -29,3 +29,9 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.title} {self.author} {self.date}'
+    
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField(unique=True)
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')

6c95d2b323ad1e5a4fc9cf20409bf2ecb4c86f70
eusoncode <eusoncode@gmail.com>
2025-04-07 23:55:02 -0600

    dockerized the app using nginx

--- django_blogs_env/bin/django-admin.py
+++ django_blogs_env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/labber/python/Django/Blogs/django_blogs_env/bin/python3
+#!/home/eustez/python/Django/Blogs/django_blogs_env/bin/python
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
